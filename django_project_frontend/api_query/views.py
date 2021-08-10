6fea8cfd42f52756c0eeae7f11746a510ec9b32f
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-10 17:28:09 +1200

    The very first working code

--- django_project_frontend/api_query/views.py
+++ django_project_frontend/api_query/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .forms import main_form
-
+from .models import Weathercache
 
 def main_view(request):
-    context = {}
-    context['form'] = main_form()
+    # context = {}
+    # context['form'] = main_form()
+    # return render(request, "index.html", context)
+    data = Weathercache.objects.using('data').all()
+    context = {'data': data}
     return render(request, "index.html", context)
