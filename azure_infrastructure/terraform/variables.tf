f4aaa55b60fcb9ae2840774f22c325097e175d89
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-11 16:26:11 +1200

    TF infra tests

--- azure_infrastructure/terraform/variables.tf
+++ azure_infrastructure/terraform/variables.tf
@@ -4,10 +4,15 @@
 variable "prefix" {
   type = string
   default = "polyarush"
-  description = "The prefix used for all resources in this example"
+  description = "The prefix used for all resources"
 }
 
 variable "location" {
-  description = "The Azure location where all resources in this example should be created"
-  default = "westeurope"
+  description = "The Azure location where all resources should be created"
+  default = "West Europe"
+}
+
+variable "rg" {
+  description = "Resource group where all resources should be created"
+  default = "EPM-RDSP"
 }
