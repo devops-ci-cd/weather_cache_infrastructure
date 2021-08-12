79c7d0a4c27e8ed8118ac85b57a79d4cc9d2a0ca
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-13 08:57:30 +1200

    ...

--- azure_infrastructure/terraform/main.tf
+++ azure_infrastructure/terraform/main.tf
@@ -17,6 +17,8 @@ resource "azurerm_mssql_server" "db_server" {
   resource_group_name          = var.rg
   location                     = var.location
   version                      = "12.0"
+
+  # get from the vault
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-adsfasdfasdf-p455wasdfasdfadsasdf0rd"
 
@@ -36,14 +38,6 @@ resource "azurerm_mssql_database" "db" {
   zone_redundant = false
   storage_account_type = "LRS"
   auto_pause_delay_in_minutes = 60
-
-  #   extended_auditing_policy {
-  #   storage_endpoint                        = "polyarush-sa01-ep1.nic.ad2e3747-8849-4e2c-a523-1cd8bed2d1ef"
-  #   storage_account_access_key              = "7BIEpmHYmlm/NtaomXQ9mtWs+Eh5djeFR/dYRSWmpVCjyOl4TXngGF27Kiu4/TEeDStOnNUtc4qTIb0vJKpw4w=="
-  #   storage_account_access_key_is_secondary = true
-  #   retention_in_days                       = 6
-  # }
-
 
   tags = {
     owner = "Evgeny_Polyarush@epam.com"
