1bec7d7ceab5f37990e4cc9764546b4198d0b0e5
devops-ci-cd <85459801+devops-ci-cd@users.noreply.github.com>
2021-08-13 11:47:28 +1200

    Update main.tf

--- azure_infrastructure/terraform/main.tf
+++ azure_infrastructure/terraform/main.tf
@@ -9,7 +9,13 @@ terraform {
 
 
 provider "azurerm" {
-  features {}
+   backend "azurerm" {
+    storage_account_name = "__terraformstorageaccount__"
+      container_name       = "terraform"
+      key                  = "terraform.tfstate"
+	  access_key  ="__storagekey__"
+    features {}
+   }
 }
 
 resource "azurerm_mssql_server" "db_server" {
