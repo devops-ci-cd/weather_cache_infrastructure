96e9248a5a81c6777fcbb74e4d2104f1721f6fce
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-20 08:41:35 +1200

    makeup

--- azure_infrastructure/terraform/main.tf
+++ azure_infrastructure/terraform/main.tf
@@ -22,10 +22,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "random_uuid" "az-id" {
