e330d79c3fbcd28099fce0a6cbdf636577e080aa
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-26 12:13:18 +1200

    reorder of the flow

--- django_project_frontend/api_query/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiQueryConfig(AppConfig):
-    name = 'api_query'
--- django_project_frontend/api_query/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-  
-# importing views from views..py
-from .views import main_view
-  
-urlpatterns = [
-    path('', main_view),
-]
