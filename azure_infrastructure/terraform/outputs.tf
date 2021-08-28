917e08785d739cf45fd76f5b456d749d328da3d3
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-29 08:56:49 +1200

    run all resources

--- azure_infrastructure/terraform/outputs.tf
+++ azure_infrastructure/terraform/outputs.tf
@@ -1,10 +1,10 @@
-# output "azure_backend_app_name" {
-#   value = azurerm_function_app.backend.name
-# }
+output "azure_backend_app_name" {
+  value = azurerm_function_app.backend.name
+}
 
-# output "azure_frontend_app_name" {
-#   value = azurerm_app_service.frontend.name
-# }
+output "azure_frontend_app_name" {
+  value = azurerm_app_service.frontend.name
+}
 
 output "azure_db_server_name" {
   value = azurerm_mssql_server.db_server.name
@@ -18,6 +18,6 @@ output "db_server_rg" {
   value = azurerm_mssql_server.db_server.resource_group_name
 }
 
-# output "queue_name" {
-#   value = azurerm_servicebus_queue.queue.name
-# }
+output "queue_name" {
+  value = azurerm_servicebus_queue.queue.name
+}
